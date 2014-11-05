b55725a9a980e52222fda0f8d8f90a2f4e4318e2
Cyril Pereira <cyril.pereira@gmail.com>
2014-11-05 01:53:19 +0100

    Updated words, refactorised lol

--- plugins/data/phrase.php
+++ plugins/data/phrase.php
@@ -7,11 +7,10 @@ return array(
   "hier j'ai acheté 500grammes de viandes et j'ai tout congelé",
   "j'en ai marre j'ai une reunion dans 5 minutes :(",
   "j'ai ouvert un blog !!!",
-  "j'ai crée un compte sur facebook dessus j'ai 3 amis, moi ma mére et mon chien",
+  "j'ai crée un compte sur facebook",
   "j'aime bien manger a la cantine",
-  "je viens de me dechirer",
-  "je pue",
   "putain encore 2 heures de taf...",
-  "putain je viens de chier, ct terrible",
-  "y a plus papier"
+  "je viens de chier, ct terrible",
+  "y a plus papier, je fais comment ?",
+  "quelqu'un pour m'aider à déménager ce week end ?",
 );
--- plugins/lol.php
+++ plugins/lol.php
@@ -12,11 +12,12 @@ class plugin_lol extends Plugin
 
     public function __construct()
     {
-
         $this->list1    = include "data/01.php";
         $this->list2    = include "data/02.php";
         $this->list3    = include "data/03.php";
         $this->list4    = include "data/04.php";
+        $this->list5    = include "data/05.php";
+        $this->list6    = include "data/06.php";
         $this->star     = include "data/star.php";
         $this->lieu     = include "data/lieu.php";
         $this->tv       = include "data/tv.php";
@@ -25,16 +26,67 @@ class plugin_lol extends Plugin
     }
 
     /**
+     * @return string
+     */
+    public function getHelp()
+    {
+        return '!lol optional (lol|random|citation|jcvd)';
+    }
+
+    /**
+     * @return array
+     */
+    public function makeCitation() {
+        $citation = include "data/citation.php";
+        return $this->array_random($citation);
+    }
+
+    /**
+     * @return array
+     */
+    public function makeJcvd() {
+        $jcvd = include "data/jcvd.php";
+        return $this->array_random($jcvd);
+    }
+
+    /**
      * @param $text
      * @return array|string
      */
-    public function getMessage($text) {
-        $rand = rand(1,2);
-        if($rand==2)
+    public function getMessage($text)
+    {
+        $commands = $this->getCommands($text);
+        $command = isset($commands[0]) ? $commands[0] : false;
+        switch($command)
         {
-            return $this->makeRandom();
-        }else{
-            return $this->makeLol();
+            case "lol":
+                return $this->makeLol();
+                break;
+            case "random":
+                return $this->makeRandom();
+                break;
+            case "jcvd":
+                return $this->makeJcvd();
+                break;
+            case "citation":
+                return $this->makeCitation();
+                break;
+            case "help":
+                return $this->getHelp();
+                break;
+            default:
+                $rand = rand(1,4);
+                if($rand==1)
+                {
+                    return $this->makeLol();
+                }elseif($rand==2){
+                    return $this->makeRandom();
+                }elseif($rand==3){
+                    return $this->makeJcvd();
+                }else{
+                    return $this->makeCitation();
+                }
+                break;
         }
     }
 
@@ -44,10 +96,12 @@ class plugin_lol extends Plugin
     private function makeRandom()
     {
         $phrase = array(
-          $this->array_random($this->list1),
+          UCFirst($this->array_random($this->list1)).",",
           $this->array_random($this->list2),
           $this->array_random($this->list3),
-          $this->array_random($this->list4)
+          $this->array_random($this->list4),
+          $this->array_random($this->list5),
+          $this->array_random($this->list6),
         );
 
         return implode(' ',$phrase);
@@ -58,13 +112,12 @@ class plugin_lol extends Plugin
      */
     public function makeLol() {
 
-        $rand = rand(1,10);
-        $numtxt = rand(1,9);
-
-        $star = $this->array_random($this->star);
-        $lieu = $this->array_random($this->lieu);
-        $tv = $this->array_random($this->tv);
-        $adj = $this->array_random($this->adj);
+        $rand   = rand(1,10);
+        $numtxt = rand(1,4);
+        $star   = $this->array_random($this->star);
+        $lieu   = $this->array_random($this->lieu);
+        $tv     = $this->array_random($this->tv);
+        $adj    = $this->array_random($this->adj);
         $phrase = $this->array_random($this->phrase);
 
         switch($rand)
@@ -82,27 +135,30 @@ class plugin_lol extends Plugin
                 $message="quelqu'un connait un bon site sur ".$star." ?";
                 break;
             case 5:
-                $message="j'ai participé a l'enregistrement de ".$tv." et ".$star." est ".$adj;
+                $message="j'ai participé à l'enregistrement de ".$tv." et ".$star." était ".$adj;
                 break;
             default:
             case 6:
                 $message=$phrase;
                 break;
             case 7:
-                $message="wow cool, y a ".$star." dans ".$tv." à ".$numtxt."H";
+                $message="wow cool, y a ".$star." sur ".$tv." à ".$numtxt."H du mat";
                 break;
             case 8:
                 $message="vous connaissez ".$star." ? ";
                 break;
             case 9:
-                $message="j'ai un faux air de ".$star;
+                $message="j'ai un faux air de ".$star." vous ne trouvez pas ?";
                 break;
             case 10:
-                $message="j'habite juste a côté, ".$lieu;
+                $message="j'habite juste a côté, ".$lieu." je croise parfois ".$star;
                 break;
         }
 
-        return $message;
+
+        $message = str_replace("de les ","des",$message);
+
+        return UCFirst($message);
     }
 }
 
