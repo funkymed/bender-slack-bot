0aec25ba91ff53a003542f3872cce46491a6def8
Cyril Pereira <cyril.pereira@gmail.com>
2016-09-24 20:57:05 +0200

    Moved getResponseMessage from controller to BenderFactory

--- src/BenderBundle/Controller/DefaultController.php
+++ src/BenderBundle/Controller/DefaultController.php
@@ -14,59 +14,15 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         $factory = $this->get('bender.factory');
+        $message = $factory->getResponseMessage();
 
-        $classes = $this->get('bender.services_chain')->getServices();
-        $help    = $this->get('bender.services_chain')->getHelps();
-
-        $factory->setClasses($classes);
-        $message    = false;
-        $text = $factory->getText();
-
-        if($factory->getUserName() && $factory->getUserName()=="slackbot")
+        if(empty($message)) {
             return new Response("");
-
-        if(empty($text))
-            return new Response("");
-
-        if(empty($help))
-            return new Response("");
-
-        //Display HELP from plugin
-        if(strstr(strtolower($text), "!help"))
-        {
-            $message = ["text"=>implode("\n",$help)];
-            //Execute plugin if triggered
         }else{
-            //Process Plugin
-            foreach($classes as $k=>$class)
-            {
-                if(strstr($text, $k))
-                {
-                    $message = $class->getMessage($text);
-                }
-            }
-            //If no message try something else funny
-            //Take a look to the qr plugin
-            if($message===false && isset($classes['!qr']))
-            {
-                $res = $classes['!qr']->getMessage($text);
-                if($res)
-                {
-                    $message=$res;
-                }
-            }
-        }
-
-        if($factory->getUserId()!='USLACKBOT' && $message!=false)
-        {
             return new JsonResponse($message);
-        }else{
-            return new Response("");
         }
-
-
     }
 }
