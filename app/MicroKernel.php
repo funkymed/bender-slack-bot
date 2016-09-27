fcb43e805d8552e0b0f1cef6e1e94d2ce2a636d3
Cyril Pereira <cyril.pereira@gmail.com>
2016-09-27 22:17:49 +0200

    Removed doctrine, swift and security bundles

--- app/MicroKernel.php
+++ app/MicroKernel.php
@@ -20,11 +20,7 @@ class MicroKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new BenderBundle\BenderBundle(),
         );
