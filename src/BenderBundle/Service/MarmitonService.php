8074fe2f2fd2c309ee308fae1b003f4768c4bb6b
Cyril Pereira <cyril.pereira@gmail.com>
2017-05-01 00:15:48 +0200

    Removed unused code

--- src/BenderBundle/Service/MarmitonService.php
+++ src/BenderBundle/Service/MarmitonService.php
@@ -34,62 +34,30 @@ class MarmitonService extends BaseService
      */
     public function checkAnswer($text)
     {
+        //Random link
         $content_page_redirect = explode('</html>', $this->get_page_content("http://www.marmiton.org/recettes/recette-hasard.aspx"));
-
         $doc = new \DOMDocument();
         $doc->loadHTML($content_page_redirect[0] . '</html>');
-
         $links = $doc->getElementsByTagName('a');
         $link_recipe = "";
         foreach ($links as $link) {
             $link_recipe = urldecode("http://www.marmiton.org" . $link->getAttribute('href'));
         }
 
-        // Fetch the recipe
+        //Parse link data
         $recipe = new Recipe($link_recipe);
         $recipe = json_decode($recipe->getRecipe());
         $message = sprintf("Pour %s personnes\n", $recipe->guests_number);
         $message .= sprintf("Préparation : %s minutes\n", $recipe->preparation_time);
         $message .= sprintf("Cuisson : %s minutes\n", $recipe->cook_time);
-        $message .= "*instructions*:\n";
+        $message .= "Instructions :\n";
         $message .= sprintf("%s\n", $recipe->instructions);
-        $message .= "*ingredients*:\n";
+        $message .= "ingredients : \n";
         foreach ($recipe->ingredients as $ingredient) {
             $message .= "\t- " . $ingredient . "\n";
         }
         return ["title" => $recipe->recipe_name, "text" => $message, "url" => $link_recipe];
-        exit;
 
-
-        $opts = array('http' => array('header' => "User-Agent:MyAgent/1.0\r\n"));
-        $context = stream_context_create($opts);
-        $content_page = file_get_contents($link_recipe, false, $context);
-
-        $doc = new \DOMDocument();
-
-        $previous_value = libxml_use_internal_errors(TRUE);
-        $doc->loadHTML((string)$content_page);
-        libxml_clear_errors();
-        libxml_use_internal_errors($previous_value);
-
-        $links = $doc->getElementsByTagName('h1');
-
-        $recette = "";
-        foreach ($links as $link) {
-            $recette = $link->nodeValue;
-
-        }
-
-        $img_recipe = "";
-        $images = $doc->getElementsByTagName('img');
-        foreach ($images as $img) {
-            if ($img->getAttribute('class') == "photo m_pinitimage") {
-                $img_recipe = $img->getAttribute('src');
-            }
-        }
-        $recette = trim(preg_replace('/\s\s+/', ' ', $recette));
-
-        return ["text" => $recette . " : " . $link_recipe, "image" => $img_recipe];
     }
 
     protected function getAnswer($message)
@@ -103,7 +71,6 @@ class MarmitonService extends BaseService
                     "footer" => "marmiton.org",
                     "footer_icon" => $this->getMediaUrl("/bundles/bender/icons/marmiton.png"),
                     "title_link" => $message['url'],
-//                    "image_url"=>$message['image'],
                     "text" => $message['text'],
                     "ts" => $date->format('U')
                 ]
